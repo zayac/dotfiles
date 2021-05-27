3b955140e1b34888c7eed477db7230ef5c14edfe
Pavel Zaichenkov <zaichenkov@gmail.com>
2021-05-27 11:43:19 +0200

    Trigger Ansible workflows from the main installation script

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 # 1. Sets up a local Python environment via virtualenv
 # 2. Installs Ansible prerequisites
 # 3. Hands off to Ansible to complete actual installation of dotfiles etc.
-#
-# On a new machine, the following commands may be required:
-#   brew install python
 
 set -e
 
@@ -17,7 +14,8 @@ ANSIBLE_ENV_SETUP=vendor/ansible/hacking/env-setup
 
 if ! command -v virtualenv &> /dev/null
 then
-  $PIP_CMD install virtualenv
+  $PIP_CMD install --user virtualenv
+  export PATH="$(python3 -m site --user-base)/bin:${PATH}"
 fi
 
 if [[ ! -e $VIRTUALENV_ACTIVATE ]]; then
@@ -32,12 +30,21 @@ if [[ -z $(pip show $PYTHON_PKGS) ]]; then
   set +e
   $PIP_CMD install $PYTHON_PKGS
   if [ $? -ne 0 ]; then
-    echo "Failed: pip install"
+    echo "Failed: $PIP_CMD install $PYTHON_PKGS"
     exit 1
   fi
   set -e
 fi
 
 source $ANSIBLE_ENV_SETUP
+
+HOST_OS=$(uname)
+
+if [ "$HOST_OS" = 'Darwin' ]; then
+  ansible-playbook --ask-become-pass -i inventory darwin.yml
+else
+  echo "Unknown host OS: $HOST_OS"
+  exit 1
+fi
 
 trap - EXIT
