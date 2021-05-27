41720564d8962039ef0104c8eab9fdb9bad515f7
Pavel Zaichenkov <zaichenkov@gmail.com>
2021-05-27 19:21:01 +0200

    Add optional flags and parameters for install.sh
    * `[roles]` parameter is introduced.
    * `--help` and `--verbose` flags are introduced.

--- install.sh
+++ install.sh
@@ -12,6 +12,52 @@ PYTHON_PKGS="PyYAML Jinja2"
 VIRTUALENV_ACTIVATE=$VIRTUALENV_TARGET_DIR/bin/activate
 ANSIBLE_ENV_SETUP=vendor/ansible/hacking/env-setup
 
+usage() {
+  echo "./install [options] [roles...]"
+  echo "Supported options:"
+  echo "  -h/--help"
+  echo "  -v/--verbose (repeat for more verbosity)"
+  echo "Other options (passed through to Ansible):"
+  echo "Supported roles:"
+  for ROLE in $(ls roles); do
+    echo "  $ROLE"
+    echo "    $(cat roles/$ROLE/description)"
+  done
+}
+
+while [ $# -gt 0 ]; do
+  if [ "$1" = '--verbose' -o "$1" = '-v' ]; then
+    VERBOSE=$((VERBOSE + 1))
+  elif [ "$1" = '--help' -o "$1" = '-h' -o "$1" = 'help' ]; then
+    usage
+    exit
+  elif [ -n "$1" ]; then
+    if [ -d "roles/$1" ]; then
+      if [ -z "$ROLES" ]; then
+        ROLES="--tags $1"
+      else
+        ROLES="$ROLES,$1"
+      fi
+    else
+      echo "Unrecognized argument(s): $*"
+      usage
+      exit 1
+    fi
+  fi
+  shift
+done
+
+if [[ $VERBOSE ]]; then
+  DEV_NULL=/dev/stdout
+  if [ $VERBOSE -gt 1 ]; then
+    echo 'Enabling extremely verbose output'
+    set -x
+  fi
+else
+  trap 'echo "Exiting: run with -v/--verbose for more info"' EXIT
+  DEV_NULL=/dev/null
+fi
+
 if ! command -v virtualenv &> /dev/null
 then
   $PIP_CMD install --user virtualenv
@@ -41,7 +87,7 @@ source $ANSIBLE_ENV_SETUP
 HOST_OS=$(uname)
 
 if [ "$HOST_OS" = 'Darwin' ]; then
-  ansible-playbook --ask-become-pass -i inventory darwin.yml
+  ansible-playbook --ask-become-pass -i inventory ${VERBOSE+-v} ${ROLES} darwin.yml
 else
   echo "Unknown host OS: $HOST_OS"
   exit 1
