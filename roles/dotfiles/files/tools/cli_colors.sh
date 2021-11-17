6cdcf243e1dee402e51ea69f884ccc8721f23d6b
Pavel Zaichenkov <zaichenkov@gmail.com>
2021-11-17 09:19:34 +0000

    add a tool for printing colors in terminal

---
+++ roles/dotfiles/files/tools/cli_colors.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+function clicolors() {
+    i=1
+    for color in {000..255}; do;
+        c=$c"$FG[$color]$color✔$reset_color  ";
+        if [ `expr $i % 8` -eq 0 ]; then
+            c=$c"\n"
+        fi
+        i=`expr $i + 1`
+    done;
+    echo $c | sed 's/%//g' | sed 's/{//g' | sed 's/}//g' | sed '$s/..$//';
+    c=''
+}
+
+clicolors
